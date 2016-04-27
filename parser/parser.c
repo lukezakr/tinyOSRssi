ae11bf3c75120a5ab5c2dd99ba1c965c805ac405
Kimbal <akimball@buffalo.edu>
2016-04-27 12:18:55 -0400

    Added files via upload
    output000 = 3x2 node network
    output001 = circular node network

---
+++ parser/parser.c
@@ -0,0 +1,291 @@
+#include <stdio.h>
+#include <string.h>
+#include "parser.h"
+
+int main(){
+	//input and output files
+	FILE *ifp;
+	FILE *ofp;
+	FILE *rfp;
+
+	ifp = fopen("../output001.txt", "r");
+	ofp = fopen("../charStringOutput001.txt","w");
+	rfp = fopen("../parsedOutput001.txt","w");
+
+	/*
+	char outputf[32];
+	char rssidumpf[32];
+	char indexf[3];
+
+	scanf("%s", indexf);
+	strcat(outputf, "../output");
+	strcat(outputf, indexf);
+	strcat(outputf,".txt");
+
+	strcat(rssidumpf, "../rssiDump");
+	strcat(rssidumpf, indexf);
+	strcat(rssidumpf,".txt");
+
+	//ifp = fopen("../output%s.txt", "r");
+	ifp = fopen(outputf, "r");
+	ofp = fopen("../charStringOutput01.txt","w");
+	//rfp = fopen("../rssiDump01.txt","w");
+	rfp = fopen(rssidumpf,"w");
+	*/
+
+	//raw character string to hold parsed text from java Listen output
+	char cdata[65536];
+	//array to hold all 400 possible edge combinations, and associated information
+	rssi rdata[20][20];
+	
+	//make sure input file exists
+	if(ifp == NULL){
+		printf("No Input File Found.\n");
+		return 0;
+	}
+
+	//remove whitespace and leading bytes
+	int size = rem_whitespace(ifp, cdata);
+	size = rem_buffer(cdata);
+
+
+	//initalize the 400 element array
+	init_rdata(rdata);
+
+	//extract rssi data from cdata and dump into rdata
+	cdata_to_rdata(cdata, rdata);
+	//change rssi decibel values to corresponding distance values
+	decibel_to_distance(rdata);
+	//print all avg rssi values from edges that hold rssi values
+	printf("\n");
+	for(int i = 0; i < 20; ++i){
+		for(int j = 0; j < 20; ++j){
+			if(rdata[i][j].set == 1){
+				printf("R = %2d, ", rdata[i][j].rmote);
+				printf("S = %2d, ", rdata[i][j].smote);
+				printf("Avg = %0.1f\n", rdata[i][j].avgrssi);
+			}
+		}
+	}
+	/*
+	for(int i = 0; i < 20; ++i){
+		for(int j = 0; j < 20; ++j){
+			if(rdata[i][j].set == 1){
+				fprintf(rfp, "R = %2d, ", rdata[i][j].rmote);
+				fprintf(rfp, "S = %2d, ", rdata[i][j].smote);
+				fprintf(rfp, "Avg = %0.1f\n", rdata[i][j].avgrssi);
+				for(int k = 0; k < rdata[i][j].counter; ++k){
+					fprintf(rfp, "\t%2d", rdata[i][j].rvals[k]);
+					if((k % 5 == 4) && (k < rdata[i][j].counter - 1)){
+						fprintf(rfp, "\n");
+					}
+				}
+				fprintf(rfp, "\n");
+			}
+		}
+	}
+	*/
+
+	//data for mapping algorithm
+	for(int i = 0; i < 20; ++i){
+		for(int j = 0; j < i; ++j){
+			if(rdata[i][j].set == 1){
+				if((rdata[i][j].set == 1) && (rdata[j][i].set == 1)){
+					fprintf(rfp, "%02d,", rdata[i][j].rmote);
+					fprintf(rfp, "%02d,", rdata[i][j].smote);
+					float average = ((float) rdata[i][j].dist + (float) rdata[j][i].dist) / 2;
+					fprintf(rfp, "%04.1f\n", average);
+				}
+			}
+		}
+	}
+	for(int i = 0; i < 20; ++i){
+		for(int j = 0; j < i; ++j){
+			if((rdata[i][j].set == 1) && (rdata[j][i].set == 1)){
+				float average = ((float) rdata[i][j].avgrssi + (float) rdata[j][i].avgrssi) / 2;
+				printf("Avg. of %2d %2d: %f\n", i, j, average);
+			}
+		}
+	}
+	fprintf(ofp, "$");
+
+
+	fclose(ifp);
+	fclose(ofp);
+	fclose(rfp);
+
+	return 0;
+}
+
+int rem_whitespace(FILE* ifp, char* cdata)
+{
+	char c;
+	int i = 0;
+
+	while(!feof(ifp)){
+		c = getc(ifp);
+		if((c != '\n') && (c != ' ')){
+			cdata[i] = c;
+			++i;
+		}
+	}
+	cdata[++i] = EOF;
+	
+	return i;
+}
+
+int rem_buffer(char* cdata)
+{
+	int n = 0;
+	int m = 0;
+
+	while(cdata[n] != EOF){
+		for(int i = 0; i < 16; ++i){
+			++n;
+		}
+		for(int i = 0; i < 8; ++i){
+			cdata[m] = cdata[n];
+			++n;
+			++m;
+		}
+		for(int i = 0; i < 6; ++i){
+			++n;
+		}
+	}
+
+	cdata[m++] = EOF;
+		
+	return m;
+
+}
+
+void init_rdata(rssi rdata[20][20])
+{
+	for(int i = 0; i < 20; ++i){
+		for(int j = 0; j < 20; ++j){
+			for(int k = 0; k < 256; ++k){
+				rdata[i][j].rvals[k] = 4096;
+			}
+			rdata[i][j].set = 0;
+			rdata[i][j].counter = 0;
+			rdata[i][j].avgrssi = 0;
+			rdata[i][j].smote = i;
+			rdata[i][j].rmote = j;
+		}
+	}
+
+	return;
+}
+
+void cdata_to_rdata(char* cdata, rssi rdata[20][20])
+{
+	int n = 0;
+	int rssi = 0;
+	int hex = 0;
+	int r = 0;
+	int s = 0;
+	while(cdata[n] != EOF){
+		for(int i = 0; i < 4; ++i){
+			if(('A' <= cdata[n]) && (cdata[n] <= 'F')){
+				hex = (cdata[n] - 'A') + 10;
+			}
+			else{
+				hex = cdata[n] - '0';
+			}
+			rssi = 16*rssi + hex;
+			++n;
+		}
+		for(int i = 0; i < 2; ++i){
+			if(('A' <= cdata[n]) && (cdata[n] <= 'F')){
+				hex = (cdata[n] - 'A') + 10;
+			}
+			else{
+				hex = cdata[n] - '0';
+			}
+			r = 16*r + hex;
+			++n;
+		}
+		for(int i = 0; i < 2; ++i){
+			if(('A' <= cdata[n]) && (cdata[n] <= 'F')){
+				hex = (cdata[n] - 'A') + 10;
+			}
+			else{
+				hex = cdata[n] - '0';
+			}
+			s = 16*s + hex;
+			++n;
+		}
+		rdata[s][r].smote = s;
+		rdata[s][r].rmote = r;
+		if(rssi >= 32768){
+			rssi = rssi - 65536;
+		}
+		rdata[s][r].rvals[rdata[s][r].counter] = rssi;
+		++rdata[s][r].counter;
+		rdata[s][r].set = 1;
+		rssi = 0;
+		s = 0;
+		r = 0;
+	}
+
+	int total = 0;
+	for(int i = 0; i < 20; ++i){
+		for(int j = 0; j < 20; ++j){
+			if(rdata[i][j].set == 1){
+				for(int k = 0; k < rdata[i][j].counter; ++k){
+					total = rdata[i][j].rvals[k] + total;
+				}
+			}
+			rdata[i][j].avgrssi = (float) total/ (float) rdata[i][j].counter;
+			total = 0;
+		}
+	}
+	return;
+}
+
+void decibel_to_distance(rssi rdata[20][20])
+{
+	int rD;
+	for(int i = 0; i < 20; ++i){
+		for(int j = 0; j < 20; ++j){
+			rD = (int) (rdata[i][j].avgrssi + 0.5);
+			if(rD >= 37)
+				rdata[i][j].dist = 1;
+			else if((rD >= 30) && (rD <= 36))
+				rdata[i][j].dist = 2;
+			else if((rD >= 26) && (rD <= 29))
+				rdata[i][j].dist = 3;
+			else if((rD >= 20) && (rD <= 25))
+				rdata[i][j].dist = 4;
+			else if((rD >= 18) && (rD <= 19))
+				rdata[i][j].dist = 5;
+			else if((rD >= 16) && (rD <= 17))
+				rdata[i][j].dist = 6;
+			else if((rD >= 10) && (rD <= 15))
+				rdata[i][j].dist = 7;
+			else if((rD >= 5) && (rD <= 9))
+				rdata[i][j].dist = 8;
+			else if((rD >= 2) && (rD <= 4))
+				rdata[i][j].dist = 9;
+			else if((rD >= 0) && (rD <= 1))
+				rdata[i][j].dist = 10;
+			else if((rD >= -3) && (rD <= -1))
+				rdata[i][j].dist = 11;
+			else if((rD >= -4) && (rD <= -4))
+				rdata[i][j].dist = 12;
+			else if((rD >= -7) && (rD <= -5))
+				rdata[i][j].dist = 13;
+			else if((rD >= -10) && (rD <= -8))
+				rdata[i][j].dist = 14;
+			else if((rD >= -11) && (rD <= -11))
+				rdata[i][j].dist = 15;
+			else if((rD >= -12) && (rD <= -12))
+				rdata[i][j].dist = 16;
+			else if((rD >= -13) && (rD <= -13))
+				rdata[i][j].dist = 17;
+			else
+				rdata[i][j].dist = 20;
+		}
+	}
+	return;
+}
---
+++ parser/parser.h
@@ -0,0 +1,25 @@
+#ifndef PARSER_H_
+#define PARSER_H_
+
+typedef struct {
+	int smote; 		//sending mote id
+	int rmote;		//receiving mote id
+	int rvals[256]; 	//rssi data values
+	int counter;
+	int set;		//1 = atleast 1 rssi value was taken, 0 = none were taken
+	float avgrssi;	//average rssi value
+	int dist;		//distance between nodes
+}rssi;
+
+//remove all whitespace in output file from java Listen and dump into char array
+int rem_whitespace(FILE* ifp, char* data);
+//remove the leading 8 bytes  from output, leaving only RSSI and two node IDs
+int rem_buffer(char* data);
+//initialize all rssi values to arbitrary number 4096 and edges set off
+void init_rdata(rssi rdata[20][20]);
+//extract data out rssi data from text string in cdata
+void cdata_to_rdata(char* cdata, rssi rdata[20][20]);
+//convert avgrssi to distance
+void decibel_to_distance(rssi rdata[20][20]);
+
+#endif
